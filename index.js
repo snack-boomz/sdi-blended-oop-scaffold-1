2856ead3f187421f31c8628f6340a5fc0ad82116
uncosung <eric.k.sung@gmail.com>
2021-07-13 14:58:37 -0700

    adds scaffolding and readme

---
+++ index.js
@@ -0,0 +1,35 @@
+class Member {
+  constructor(name) {
+
+  };
+};
+class BankAccount {
+  constructor(member) {
+
+  };
+  credit() {
+
+  };
+  debit() {
+
+  };
+  checkBalance() {
+
+  };
+};
+class CheckingAccount {
+  constructor(member) {
+
+  };
+}
+class SavingsAccount {
+  constructor(member) {
+
+  };
+}
+const distributeEvenly = () => {
+
+};
+const distributeToSavings = () => {
+
+};
